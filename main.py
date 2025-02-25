06632ca8e0a26e89da1b0f78a4110d7126e2284c
ppastorf <pdrpfernandes99@gmail.com>
2025-02-25 14:11:32 -0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchtext
+import os
+import collections
 
+os.makedirs('./data', exist_ok=True)
+
+train_dataset, test_dataset = torchtext.datasets.AG_NEWS(root='./data')
+
+classes = ['World', 'Sports', 'Business', 'Sci/Tech']
+
+print(train_dataset)
+print(test_dataset)
+print(next(train_dataset))
